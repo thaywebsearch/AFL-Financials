19c16c8662a4c8479440a46aa692814ae91ed736
Gilberto <75209226+thaywebsearch@users.noreply.github.com>
2021-08-14 15:29:02 +0100

    Create f_app_mainpage.py

---
+++ myapp/f_app_mainpage.py
@@ -0,0 +1,21 @@
+###################
+# Set up main app #
+###################
+
+# Plot the prices and the bolinger bands
+st.write('Stock Bollinger bands')
+st.line_chart(bb)
+
+progress_bar = st.progress(0)
+
+# Plot MACD
+st.write('Stock Moving Average Convergence Divergence (MACD)')
+st.area_chart(macd)
+
+# Plot RSI
+st.write('Stock RSI ')
+st.line_chart(rsi)
+
+# Data of recent days
+st.write('Recent data ')
+st.dataframe(df.tail(10))

d217a4a3d121785e21b3e5c8f49a73ec66f5487c
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2021-08-19 22:06:21 +0100

    WebS_stockpru update

---
+++ PRU Financials/WebS_stockpru.py
@@ -0,0 +1,12 @@
+import re
+import json
+import csv
+from io import StringIO
+from bs4 import BeautifulSoup
+import requests
+
+url_stats = 'https://finance.yahoo.com/quote/{}/key-statistics?p={}'
+url_profile = 'https://finance.yahoo.com/quote/{}/profile?p={}'
+url_financials = 'https://finance.yahoo.com/quote/{}/financials?p={}'
+
+stock = 'PRU'

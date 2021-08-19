c33d7bc678840a0c38269a64c2e6a25e0e5591aa
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2021-08-19 22:14:35 +0100

    WebS_stockpru update V1

--- PRU Financials/WebS_stockpru.py
+++ PRU Financials/WebS_stockpru.py
@@ -10,3 +10,12 @@ url_profile = 'https://finance.yahoo.com/quote/{}/profile?p={}'
 url_financials = 'https://finance.yahoo.com/quote/{}/financials?p={}'
 
 stock = 'PRU'
+
+response = requests.get(url_financials.format(stock, stock))
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+pattern = re.compile(r'\s--\sData\s--\s')
+script_data = soup.find('script', text=pattern).contents[0]
+
+script_data[:500]

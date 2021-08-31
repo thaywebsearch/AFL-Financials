e8e29e3afc95619fcf0187ec85ce99d6bfbd8411
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2021-08-31 22:25:45 +0100

    STAG datareader

---
+++ myapp/STAG_datareader_adj_close.py
@@ -0,0 +1,5 @@
+from pandas_datareader import data
+
+stag = data.DataReader('STAG', start='2020-01-01', end='2020-12-31', data_source='yahoo')['Adj Close']
+
+stag.plot(title='STAG Adj.Closing Price')

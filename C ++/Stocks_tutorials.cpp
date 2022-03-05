0c59c28a0275e1500cb91c1deef3f09ff73be23a
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-03-05 19:58:02 +0000

    stocks tutorials

---
+++ C ++/Stocks_tutorials.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Stocks {
+    public:
+    string industry;
+    string sector;
+    int ticker;
+};
+
+int main () {
+
+    Stocks stocksObj1;
+    stocksObj1.industry = "Beverages-Non Alcoholic";
+    stocksObj1.sector = "Consumer Defensive";
+    stocksObj1.ticker = "PEP";
+
+    Stocks stocksObj2;
+    stocksObj2.industry = "Beverages-Non Alcoholic";
+    stocksObj2.sector = "Consumer Defensive";
+    stocksObj2.ticker = "KO";
+
+     cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.ticker << "\n";
+    cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.ticker << "\n";
+    return 0;
+}
+
+
+
+

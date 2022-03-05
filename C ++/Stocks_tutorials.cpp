b62e1e470d3599839cf83f37ce72287d2e63c67e
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-03-05 20:11:48 +0000

    ok

--- C ++/Stocks_tutorials.cpp
+++ C ++/Stocks_tutorials.cpp
@@ -6,7 +6,7 @@ class Stocks {
     public:
     string industry;
     string sector;
-    int ticker;
+    int Company;
 };
 
 int main () {
@@ -14,15 +14,15 @@ int main () {
     Stocks stocksObj1;
     stocksObj1.industry = "Beverages-Non Alcoholic";
     stocksObj1.sector = "Consumer Defensive";
-    stocksObj1.ticker = PEP;
+    stocksObj1.Company = PepsiCo Inc;
 
     Stocks stocksObj2;
     stocksObj2.industry = "Beverages-Non Alcoholic";
     stocksObj2.sector = "Consumer Defensive";
-    stocksObj2.ticker = KO;
+    stocksObj2.Company = Coca Cola Co;
 
-     cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.ticker << "\n";
-    cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.ticker << "\n";
+     cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.Company << "\n";
+    cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.Company << "\n";
     return 0;
 }
 

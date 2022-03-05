040e5f70f6e97eddbcf55f8c8cda1144a7465c99
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-03-05 20:01:58 +0000

    new update stocksObj.ticker

--- C ++/Stocks_tutorials.cpp
+++ C ++/Stocks_tutorials.cpp
@@ -14,12 +14,12 @@ int main () {
     Stocks stocksObj1;
     stocksObj1.industry = "Beverages-Non Alcoholic";
     stocksObj1.sector = "Consumer Defensive";
-    stocksObj1.ticker = "PEP";
+    stocksObj1.ticker = PEP;
 
     Stocks stocksObj2;
     stocksObj2.industry = "Beverages-Non Alcoholic";
     stocksObj2.sector = "Consumer Defensive";
-    stocksObj2.ticker = "KO";
+    stocksObj2.ticker = KO;
 
      cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.ticker << "\n";
     cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.ticker << "\n";

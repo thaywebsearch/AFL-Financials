559f7670128d36922e3c68d162fd31ca1d891e67
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-03-06 01:46:50 +0000

    update date former

--- C ++/Stocks_tutorials.cpp
+++ C ++/Stocks_tutorials.cpp
@@ -16,13 +16,13 @@ int main () {
     stocksObj1.industry = "Beverages-Non Alcoholic";
     stocksObj1.sector = "Consumer Defensive";
     stocksObj1.company ="Coca Cola Co";
-    stocksObj1.year = 1999;
+    stocksObj1.year = 1886;
 
     Stocks stocksObj2;
     stocksObj2.industry = "Beverages-Non Alcoholic";
     stocksObj2.sector = "Consumer Defensive";
     stocksObj2.company ="Pepsico Inc";
-    stocksObj2.year = 1969;
+    stocksObj2.year = 1965;
 
     cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.company << " " << stocksObj1.year << "\n";
     cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.company << " " << stocksObj2.year << "\n";

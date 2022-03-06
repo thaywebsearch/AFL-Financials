5aacd821c0a890975382e6775d2a5936ff157f43
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-03-06 01:42:13 +0000

    stocks tutorials success update version1.3

--- C ++/Stocks_tutorials.cpp
+++ C ++/Stocks_tutorials.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 class Stocks {
     public:
+    string company;
     string industry;
     string sector;
-    int Company;
+    int year;
 };
 
 int main () {
@@ -14,15 +15,17 @@ int main () {
     Stocks stocksObj1;
     stocksObj1.industry = "Beverages-Non Alcoholic";
     stocksObj1.sector = "Consumer Defensive";
-    stocksObj1.Company = PepsiCo Inc;
+    stocksObj1.company ="Coca Cola Co";
+    stocksObj1.year = 1999;
 
     Stocks stocksObj2;
     stocksObj2.industry = "Beverages-Non Alcoholic";
     stocksObj2.sector = "Consumer Defensive";
-    stocksObj2.Company = Coca Cola Co;
+    stocksObj2.company ="Pepsico Inc";
+    stocksObj2.year = 1969;
 
-     cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.Company << "\n";
-    cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.Company << "\n";
+    cout << stocksObj1.industry << " " << stocksObj1.sector << " " << stocksObj1.company << " " << stocksObj1.year << "\n";
+    cout << stocksObj2.industry << " " << stocksObj2.sector << " " << stocksObj2.company << " " << stocksObj2.year << "\n";
     return 0;
 }
 

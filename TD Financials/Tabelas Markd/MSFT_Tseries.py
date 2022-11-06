97ca406efe510453dac05155002b1152c7a3ef71
Gilberto Pereira <gilbertopereira_123@hotmail.com>
2022-11-06 20:00:39 +0000

    timeseries

---
+++ TD Financials/Tabelas Markd/MSFT_Tseries.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+api_key = 'E0DBWOY6S1NS2GV5'
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+print(data)
+
+i = 1
+
+close_data = data['4.close']
+percentage_change = close_data.pct_change()
+
+print(percentage_change)
+
+last_change = percentage_change[-1]
+
+if abs(last_change) > 0.0004:
+    print("MSFT Alert:" + str(last_change))
